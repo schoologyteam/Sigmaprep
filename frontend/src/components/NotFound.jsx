6554edf07cfcfa908f69e48964c643d7b28ebdf3
Maddox Schmidlkofer <maddoxschmidlkofer@gmail.com>
2024-10-11 16:36:49 -0400

    fix

--- frontend/src/components/NotFound.jsx
+++ frontend/src/components/NotFound.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Container, Header, Icon, Segment, Button, Image } from 'semantic-ui-react';
-import { useNavigate } from 'react-router-dom';
 import quack from '/img/quackprep_logo.webp';
+import { changeNavbarPage } from './navbar/navbarSlice';
+import { useDispatch } from 'react-redux';
 
 const NotFoundPage = () => {
-  const navigate = useNavigate();
+  const dispatch = useDispatch();
 
   return (
     <Container text>
@@ -18,7 +19,7 @@ const NotFoundPage = () => {
           <p>We're sorry, but the page you're looking for doesn't exist.</p>
           <p>You may have mistyped the address or the page may have moved.</p>
         </Segment.Inline>
-        <Button primary onClick={() => navigate('/')} style={{ marginTop: '2em' }}>
+        <Button primary onClick={() => dispatch(changeNavbarPage('/'))} style={{ marginTop: '2em' }}>
           Go to Homepage
         </Button>
       </Segment>
