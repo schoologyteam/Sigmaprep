612687cd725805c68b399d0bd43166c29e25334a
maddox05 <maddoxschmidlkofer@gmail.com>
2024-10-23 19:06:03 -0400

    allow bots in

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,5 +1,33 @@
 import { checkApiKey } from "#models/auth/index.js";
 
+/**
+ *
+ * @param {String} useragent
+ * @returns {boolean}
+ */
+function checkUserAgentIsBot(useragent) {
+  if (useragent == null) {
+    return false;
+  }
+  const agents = {
+    googlebot: "googlebot",
+    bingbot: "bingbot",
+    slurp: "slurp", // Yahoo bot
+    duckduckbot: "duckduckbot",
+    baiduspider: "baiduspider",
+    yandex: "yandex",
+    sogou: "sogou",
+    exabot: "exabot",
+    facebot: "facebot", // Facebook
+    ia_archiver: "ia_archiver", // Wayback Machine
+  };
+
+  if (useragent.toLowerCase() in agents) {
+    return true;
+  }
+  return false;
+}
+
 export async function isAuthenticated(req, res, next) {
   if (req.user && req.isAuthenticated()) {
     //dlog("user is logged in");
@@ -8,8 +36,12 @@ export async function isAuthenticated(req, res, next) {
     dlog("api token detected");
     req.user = await checkApiKey(req.headers.token); // inject into req.user temporaryily
     next();
+  } else if (checkUserAgentIsBot(req.get("User-Agent"))) {
+    dlog("bot detected");
+    req.user = 23; // web crawler id
+    next();
   } else {
-    //dlog("User not authed");
+    dlog("User not authed");
     res.status(401).json({
       message: "401 you do not have access to this page, please login",
     });
