474590aaa8a8a50ed35eafa35dd5bd9f61da0a23
Maddox Schmidlkofer <maddoxschmidlkofer@gmail.com>
2024-10-26 21:58:25 -0400

    Update index.js

--- backend/models/question/index.js
+++ backend/models/question/index.js
@@ -5,7 +5,7 @@ export async function getQuestionsByGroupId(group_id, type) {
   return await sqlExe.executeCommand(
     // PULL IN MORE! TODO WTF TEST SPENT10 MIN
     `SELECT q.id, q.question, g.id as group_id, gt.type_name as type FROM questions q 
-    JOIN group_question gq ON gq.group_id = 3 AND gq.question_id = q.id 
+    JOIN group_question gq ON gq.group_id = :group_id AND gq.question_id = q.id 
     JOIN cgroups g ON gq.group_id = g.id 
     JOIN group_types gt ON gt.id = g.type 
     WHERE q.deleted = 0
