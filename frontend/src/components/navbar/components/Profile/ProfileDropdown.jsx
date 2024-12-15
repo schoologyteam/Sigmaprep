5d4831e35040c14f9281ce17aef2abb374708984
maddox05 <maddoxschmidlkofer@gmail.com>
2024-12-15 11:14:11 -0500

    loggg

--- frontend/src/components/navbar/components/Profile/ProfileDropdown.jsx
+++ frontend/src/components/navbar/components/Profile/ProfileDropdown.jsx
@@ -40,12 +40,14 @@ export default function ProfileDropdown({ activePage, handlePageChange, hasStrea
             open={confimOpen}
             onSubmit={() => {
               dispatch(signOut());
+              console.log('sign out');
               setTimeout(() => {
                 navigate('/home'); // was using my changeNavPage but it didnt run fast enough I Dont think
 
                 setConfirmOpen(false);
 
                 navigate(0); // reloads page so store is reset should null it all out to be more performant
+                console.log('reset to hompage');
               }, 500);
             }}
           />
