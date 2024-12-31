3e1fdd8aa391487227c4d45e59e9d9dfb27a1adf
maddox05 <maddoxschmidlkofer@gmail.com>
2024-12-31 10:02:46 -0500

    saving ai work

--- backend/models/question/ai/index.js
+++ backend/models/question/ai/index.js
@@ -1,3 +1,10 @@
 import sqlExe from "#db/dbFunctions.js";
+import { openai } from "#config/config";
 
-export function generateQuestionLike(user_id, likeQuestion) {}
+export async function generateQuestionLike(user_id, likeQuestion) {
+  const quackAssist = await openai.beta.assistants.retrieve(
+    "asst_a168JvA9PlzK2WaKZ6oukDe4"
+  );
+
+  // start a thread here
+}
