8ac0d714f1dbec961d93d3864f86ae8d93e28329
maddox05 <maddoxschmidlkofer@gmail.com>
2025-01-10 15:14:00 -0500

    eedit

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
   return {
     // Uncomment if you need the build directory customization
     // build: { outDir: '../backend/public', emptyOutDir: true },
