3044e091feceec518d2e27e5928bf3f69427369b
maddox05 <maddoxschmidlkofer@gmail.com>
2025-01-19 18:26:28 -0500

    save

--- backend/models/group/ai/index.js
+++ backend/models/group/ai/index.js
@@ -5,51 +5,47 @@ import FormData from "form-data";
 import { sleep } from "#utils/utils.js";
 
 /**
- * Keeps retrying until conversion is completed if it retries more than 5 times, it will throw an error
+ * Keeps retrying until conversion is completed. if it retries more than 5 times, it will throw an error
  * @param {String} pdf_id id of pdf
  * @param {void} retries do not put in here pal
  * @param {String} format .md | .mmd | .html | .docx
- *
+ * @returns {String} formatted pdf in given format
  */
 export async function getFormattedPdfByPdfIdMathpix(
   pdf_id,
-
   format = ".md",
   retries = 0
 ) {
-  try {
-    if (retries > 5) {
-      throw new Error("failed to get pdf by pdf_id. retries exceeded");
+  if (retries > 5) {
+    throw new Error("failed to get pdf by pdf_id. retries exceeded");
+  }
+  const result = await axios.get(
+    `https://api.mathpix.com/v3/converter/${pdf_id}`,
+    {
+      headers: {
+        app_id: MATHPIX_API_INFO.MATHPIX_APP_ID,
+        app_key: MATHPIX_API_INFO.MATHPIX_API_KEY,
+        "Content-type": "multipart/form-data",
+      },
     }
-    const result = await axios.get(
-      `https://api.mathpix.com/v3/converter/${pdf_id}`,
+  );
+  if (result.data?.status === "completed") {
+    const mmd = await axios.get(
+      `https://api.mathpix.com/v3/pdf/${pdf_id}${format}`,
       {
         headers: {
           app_id: MATHPIX_API_INFO.MATHPIX_APP_ID,
           app_key: MATHPIX_API_INFO.MATHPIX_API_KEY,
-          "Content-type": "multipart/form-data",
         },
       }
     );
-    //   dlog(result.data?.status);
-    if (result.data?.status === "completed") {
-      const mmd = await axios.get(
-        `https://api.mathpix.com/v3/pdf/${pdf_id}${format}`,
-        {
-          headers: {
-            app_id: MATHPIX_API_INFO.MATHPIX_APP_ID,
-            app_key: MATHPIX_API_INFO.MATHPIX_API_KEY,
-          },
-        }
-      );
-      return mmd.data;
-    } else {
-      await sleep(1000); // wait one second
-      return getFormattedPdfByPdfIdMathpix(pdf_id, format, retries + 1);
-    }
-  } catch (error) {
-    console.error(error);
-    throw error;
+    return mmd.data;
+  } else if (result.data?.status === "error") {
+    throw new Error("failed to get pdf by pdf_id. status is error");
+  } else {
+    dlog("status not completed. retrying in 10 seconds");
+    await sleep(10000); // wait 10 second
+    return getFormattedPdfByPdfIdMathpix(pdf_id, format, retries + 1);
   }
 }
 
@@ -61,29 +57,23 @@ export async function getFormattedPdfByPdfIdMathpix(
  * @returns {String} pdf_id
  */
 export async function postPDFToMathpix(formData) {
-  try {
-    const result = await axios.post(
-      "https://api.mathpix.com/v3/pdf",
-      formData,
+  const result = await axios.post(
+    "https://api.mathpix.com/v3/pdf",
+    formData,
 
-      {
-        headers: {
-          ...formData.getHeaders(),
-          app_id: MATHPIX_API_INFO.MATHPIX_APP_ID,
-          app_key: MATHPIX_API_INFO.MATHPIX_API_KEY,
-          "Content-type": "multipart/form-data",
-        },
-      }
-    );
-    if (!result.data.pdf_id) {
-      throw new Error("failed to parse pdf");
-      return;
+    {
+      headers: {
+        ...formData.getHeaders(),
+        app_id: MATHPIX_API_INFO.MATHPIX_APP_ID,
+        app_key: MATHPIX_API_INFO.MATHPIX_API_KEY,
+        "Content-type": "multipart/form-data",
+      },
     }
-    return result.data.pdf_id;
-  } catch (error) {
-    console.error(error);
-    throw error;
+  );
+  if (!result.data.pdf_id) {
+    throw new Error("failed to parse pdf");
   }
+  return result.data.pdf_id;
 }
 
 export async function parsePdfIntoGroup(formData, class_id, user_id) {
@@ -94,10 +84,11 @@ export async function parsePdfIntoGroup(formData, class_id, user_id) {
     dlog(mmd);
   } catch (error) {
     console.error(error);
-    throw error;
+    throw error; // throw to route to handle it
   }
 }
 
+// testing
 const formData = new FormData();
 formData.append(
   "file",
