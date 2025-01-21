be01b65553a34545b96e73b888c8df85b6e473b8
maddox05 <maddoxschmidlkofer@gmail.com>
2025-01-21 07:27:44 -0500

    better more prof

--- frontend/src/app/about/About.jsx
+++ frontend/src/app/about/About.jsx
@@ -4,7 +4,8 @@ import CreatorInfo from './CreatorInfo.jsx';
 const About = () => {
   const creators = [
     {
-      imgSrc: '/img/team/madgoof.jpeg',
+      imgSrc:
+        'https://media.licdn.com/dms/image/v2/D5603AQFo1LTbAVeJ-A/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1707747042254?e=1743033600&v=beta&t=sEuGzlRHda7qrMS_ROE_EYWo09FRDSHtzlOL_VGL_r4',
       name: 'Maddox Schmidlkofer',
       description: 'All work and no play makes maddox a dull boy.', //Founder and Lead Developer of QuackPrep. Passionate about creating innovative solutions to make learning fun and engaging for college students.
       links: [
@@ -13,9 +14,10 @@ const About = () => {
       ],
     },
     {
-      imgSrc: '/img/team/haygoof.jpeg',
+      imgSrc:
+        'https://media.licdn.com/dms/image/v2/D5603AQH9l-dAsT1WXQ/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1699241824248?e=1743033600&v=beta&t=T7FaGrM_q1ogj_G6X7_BKCTkj5WDnswVCXIJrtqlb2U',
       name: 'Hayes Bounds',
-      description: '🌾', //Founder and Lead Developer of QuackPrep. Passionate about creating innovative solutions to make learning fun and engaging for college students.
+      description: 'Think about that', //Founder and Lead Developer of QuackPrep. Passionate about creating innovative solutions to make learning fun and engaging for college students.
       links: [
         { label: 'GitHub', url: 'https://github.com/hebounds', icon: 'github', color: 'black' },
         { label: 'LinkedIn', url: 'https://www.linkedin.com/in/hayes-bounds-441a7b210/', icon: 'linkedin', color: 'blue' },
