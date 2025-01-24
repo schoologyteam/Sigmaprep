34cd51d8f9deeb30a434861aeb5f79c978e60177
maddox05 <maddoxschmidlkofer@gmail.com>
2025-01-24 14:05:31 -0500

    seo and removed plus button as its shit

--- frontend/src/app/layout/brandlogo/BrandLogo.jsx
+++ frontend/src/app/layout/brandlogo/BrandLogo.jsx
@@ -1,13 +1,13 @@
 import { Image, Menu } from 'semantic-ui-react';
 import './BrandLogo.css';
 import { changeNavbarPage } from '@app/layout/navbar/navbarSlice';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
-export default function BrandLogo({}) {
+export default function BrandLogo() {
   const navigate = useNavigate();
   return (
-    <Menu.Item as={'a'} header onClick={() => changeNavbarPage(navigate, '/')}>
-      <Image className='logo' size='mini' src='/img/quackprep_logo.webp' />
+    <Menu.Item as={Link} href='/' header onClick={() => changeNavbarPage(navigate, '/')}>
+      <Image alt='quackprep duck' className='logo' size='mini' src='/img/quackprep_logo.webp' />
       <span className='quackprep' style={{ fontSize: '1.2rem' }}>
         QuackPrep
       </span>
