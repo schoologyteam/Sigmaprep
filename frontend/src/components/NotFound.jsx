7c98f1eae790ea2cc57bb58af1ded7b0bf6b6455
maddox05 <maddoxschmidlkofer@gmail.com>
2025-01-25 09:25:05 -0500

    whyyy

--- frontend/src/components/NotFound.jsx
+++ frontend/src/components/NotFound.jsx
@@ -9,20 +9,51 @@ const NotFoundPage = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
+  const handleHomepageClick = () => {
+    dispatch(changeNavbarPage(navigate, '/'));
+  };
+
   return (
     <Container text>
-      <Segment placeholder padded='very' textAlign='center'>
+      <Segment
+        placeholder
+        padded='very'
+        textAlign='center'
+        style={{ animation: 'shake 0.5s', borderRadius: '20px', boxShadow: '0 4px 15px rgba(0, 0, 0, 0.2)' }}
+      >
         <Header icon>
-          {/* <Icon name='search' /> */}
-          <Image size='massive' as={'img'} src={quack}></Image>
-          <br></br> 404 - Page Not Found
+          <Image
+            size='massive'
+            as={'img'}
+            src={quack}
+            style={{ filter: 'drop-shadow(2px 2px 5px rgba(0, 0, 0, 0.5))' }}
+            alt='QuackPrep Logo'
+          />
+          <div style={{ fontSize: '2.5em', marginTop: '0.5em', marginBottom: '.3em', color: '#2D89FF' }}>
+            404 - Page Not Found
+          </div>
         </Header>
         <Segment.Inline>
-          <p>We're sorry, but the page you're looking for doesn't exist.</p>
-          <p>You may have mistyped the address or the page may have moved.</p>
+          <p style={{ fontSize: '1.2em', color: '#333', marginBottom: '1.5em' }}>
+            We're sorry, but the page you're looking for doesn't exist.
+          </p>
+          <p style={{ fontSize: '1em', color: '#555' }}>You may have mistyped the address or the page may have moved.</p>
         </Segment.Inline>
-        <Button primary onClick={() => dispatch(changeNavbarPage(navigate, '/'))} style={{ marginTop: '2em' }}>
-          Go to Homepage
+        <Button
+          primary
+          onClick={handleHomepageClick}
+          style={{
+            marginTop: '2em',
+            padding: '1em 2em',
+            fontSize: '1.1em',
+            borderRadius: '12px',
+            backgroundColor: '#FFA500',
+            color: '#fff',
+          }}
+          animated='fade'
+        >
+          <Button.Content visible>Go to Homepage</Button.Content>
+          <Button.Content hidden>Take Me Back!</Button.Content>
         </Button>
       </Segment>
     </Container>
