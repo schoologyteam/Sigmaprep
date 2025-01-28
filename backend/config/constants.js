60d18934c57dbff9d2bd7edc13f17e4899dccd2b
Maddox Schmidlkofer <maddoxschmidlkofer@gmail.com>
2025-01-28 10:20:06 -0500

    Update constants.js

--- backend/config/constants.js
+++ backend/config/constants.js
@@ -4,7 +4,7 @@ export const MAX_QUESTIONS_CONTEXT = 10;
 export const QUACK_GEN_QUESTION_ASS_ID = "asst_a168JvA9PlzK2WaKZ6oukDe4";
 export const MAX_PROMPT_LENGTH = 15000; // hopefully a pdf parse is no longer than this.
 export const MAX_USER_PROMPT_LENGTH = 1000;
-export const MATHPIX_API_PDF_GET_RESULT_RETRIES = 3;
+export const MATHPIX_API_PDF_GET_RESULT_RETRIES = 10;
 export const QUACK_CREAT_GROUP_ASS_ID = "asst_UXDbP8qIkOJw50jN9OLp36oA";
 
 export const MAX_USERNAME_LENGTH = 25;
