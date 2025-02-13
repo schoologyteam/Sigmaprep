ebd18a6cb3694bdd5512865e22e1caad8d585627
maddox05 <maddoxschmidlkofer@gmail.com>
2025-02-13 15:13:37 -0500

    comments etc

---
+++ backend/database/migrate.js
@@ -0,0 +1,3 @@
+// TODO
+// go from oldest sql file to newest applying each to database?
+// what id database alr has changes? how to know what changes it needs?
--- backend/database/seed.js
+++ backend/database/seed.js
@@ -13,11 +13,19 @@ export const class_categories = [
   { name: "STAT", description: "Statistics" },
 ];
 
+export const group_types = [{ type_name: "topic" }, { type_name: "exam" }];
+
 export async function seedDb() {
   for (const category of class_categories) {
     await sqlExe.executeCommand(
       `INSERT INTO class_categories (name, description) VALUES (:name, :description)`,
       { name: category.name, description: category.description }
+    );
+  }
+  for (const type of group_types) {
+    await sqlExe.executeCommand(
+      `INSERT INTO group_tyes (type_name) VALUES (:type_name)`,
+      { type_name: type.type_name }
     );
   }
   await sqlExe.executeCommand(
