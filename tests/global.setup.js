e8fbfbbd0d1442619f084a12466145131a027bba
maddox05 <maddoxschmidlkofer@gmail.com>
2025-02-14 14:52:31 -0500

    try test

--- tests/global.setup.js
+++ tests/global.setup.js
@@ -1,5 +1,10 @@
 import { initTestAccount } from "@backend/models/auth";
 
 export default async function globalSetup() {
-  await initTestAccount();
+  try {
+    await initTestAccount();
+  } catch (error) {
+    console.log("test account may already exists. going forward.");
+    console.error(error);
+  }
 }
